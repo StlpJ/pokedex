2cc5cc2ead0a926502d5eba8dbcac6508fb54266
FIRST_NAME LAST_NAME <julpaultes@gmail.com>
2025-10-19 11:44:00 +0200

    ajout dun pokemon mystere

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,4 +1,4 @@
-function PokemonCard() {
+/*function PokemonCard() {
   return (
     <figure>
       <img
@@ -8,6 +8,31 @@ function PokemonCard() {
       <figcaption>Bulbasaur</figcaption>
     </figure>
   );
+}
+  */
+
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
+function PokemonCard() {
+  const pokemon = pokemonList[1];
+  return pokemon.imgSrc !== undefined ? (
+    <figure>
+      <img src={pokemon.imgSrc} alt={pokemon.name} />
+      <figcaption>{pokemon.name}</figcaption>
+    </figure>
+  ) : (
+    <p>???</p>
+  );
+  // {pokemon.imgSrc !== undefined ? (<img src={pokemon.imgSrc} alt={pokemon.name}/>) : (<p>???</p>)}
 }
 
 export default PokemonCard;
