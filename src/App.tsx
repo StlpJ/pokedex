6da713544c313fb193872b3b8e97a2560061e9b3
FIRST_NAME LAST_NAME <julpaultes@gmail.com>
2025-10-21 17:44:06 +0200

    ajout de boutons pour choisir le pokemon

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 import PokemonCard from "./components/PokemonCard";
 
@@ -13,7 +14,35 @@ const pokemonList = [
 ];
 
 function App() {
-  return <PokemonCard pokemon={pokemonList[0]} />;
+  const [pokemonName, setPokemonName] = useState("bulbasaur");
+
+  const pokemon = pokemonList.find((pokemon) => pokemon.name === pokemonName);
+
+  if (pokemon == null) {
+    throw new Error("Invalid pokemon name");
+  }
+
+  return (
+    <div>
+      <button type="button" onClick={() => setPokemonName("bulbasaur")}>
+        Bulbasaur
+      </button>
+      <button type="button" onClick={() => setPokemonName("mew")}>
+        Mew
+      </button>
+      <PokemonCard pokemon={pokemon} />
+    </div>
+  );
 }
 
 export default App;
+
+/*
+  const [user, setUser] = useState({ name: "Bulbasaur" });
+
+  const handleClick = () => {
+    // user.name = "Alice"; => NO !!!
+    setUser({ name: "Alice" });
+  };
+
+*/
